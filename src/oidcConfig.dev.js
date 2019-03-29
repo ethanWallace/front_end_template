1103e657653e087aa54929b4e8298a527e67be45
steph4104 <stephanie4104@gmail.com>
2019-03-29 09:43:05 -0400

    fix typo error in oidcConfig

--- src/oidcConfig.dev.js
+++ src/oidcConfig.dev.js
@@ -5,7 +5,7 @@ module.exports = {
   client_id: '123456',
   client_secret:
   'shhhhhhhh-its-a-secret-to-everyone-replace-me',
-  scope: 'openid YOUR_SCOPES_HERE,
+  scope: 'openid YOUR_SCOPES_HERE',
   post_logout_redirect_uri: `${url}/#!logout`,
   redirect_uri: `${url}/#!callback`,
   silent_redirect_uri: `${url}/#!silent`,
